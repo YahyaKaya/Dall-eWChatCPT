9e196f30033eff9e74810241e455714c71b22142
Yahya Kaya <117475209+YahyaKaya@users.noreply.github.com>
2023-03-04 23:15:25 +0300

    Add files via upload

---
+++ Dall-EWChatCPT-1.py
@@ -0,0 +1,48 @@
+import openai
+import requests
+import wget
+import cv2
+
+text = input("User: ")
+
+openai.api_key = "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"
+
+endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 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"
+}
+
+data = {
+    "prompt": f"I want dall-e to draw this {text} (write it without any commentary)",
+    "max_tokens": 2048,
+    "n": 1,
+    "stop": None,
+    "temperature": 1
+}
+
+def draw(text):
+    response1 = requests.post(endpoint, headers=headers, json=data)
+           
+    if response1.status_code == 200:
+        return response1.json()["choices"][0]["text"]
+    else:
+        print(response1.status_code)
+        return "Failed"
+
+response2 = openai.Image.create(
+    model="image-alpha-001",
+    prompt=draw(text)
+)
+
+url = response2["data"][0]["url"]
+
+print(draw(text))
+print("\n")
+print(url)
+
+filename = wget.download(url)
+np_image = cv2.imread(filename)
+cv2.imshow('image', np_image)
+cv2.waitKey()
---
+++ Dall-eWChatCPT-2.py
@@ -0,0 +1,55 @@
+import openai
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import urllib.request as urllib
+
+text = input("User: ")
+
+openai.api_key = "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"
+
+endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 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"
+}
+
+data = {
+    "prompt": f"I want dall-e to draw {text}, can you write it in a way that Dall-e would understand (write it without any commentary)",
+    "max_tokens": 2048,
+    "n": 1,
+    "stop": None,
+    "temperature": 1
+}
+
+def draw(text):
+    response1 = requests.post(endpoint, headers=headers, json=data)
+           
+    if response1.status_code == 200:
+        return response1.json()["choices"][0]["text"]
+    else:
+        print(response1.status_code)
+        return "Failed"
+
+response2 = openai.Image.create(
+    model="image-alpha-001",
+    prompt=draw(text)
+)
+
+url = response2["data"][0]["url"]
+
+print(draw(text))
+print("\n")
+print(url)
+print("\n")
+
+def url_to_image(url):
+	resp = urllib.urlopen(url)
+	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	return image
+
+pltimg = plt.imshow(url_to_image(url))
+plt.show()
